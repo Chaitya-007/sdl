9f3d8f19c2b3861fa94ddbb3f6b21801615bf1fa
Chaitya <chaitanyaasole@gmail.com>
2024-04-24 20:17:22 +0530

    done with second assignment

---
+++ 02_Alert/script.js
@@ -0,0 +1,35 @@
+const humanlife = 80;
+const weeksinyear = 52;
+const averagelifespan = humanlife*weeksinyear;
+
+// ! Use capital Date
+const now = new Date();
+
+// ! Use the same variable for getting the hours
+const time = now.getHours();
+
+let duration;
+
+if(time < 12)
+{
+    duration = "morning";
+}
+else if(time < 18)
+{
+    duration = "afternoon";
+}
+else
+{
+    duration = "night";
+}
+
+alert(`the average life span of human is : ${averagelifespan}\n and duration is ${duration}`);
+
+const outputhtml = document.getElementById('output');
+
+
+outputhtml.innerHTML = 
+`<p> the average life span of human is : ${averagelifespan}\n and duration is ${duration} </p>`
+
+
+
