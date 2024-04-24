def824ea5829050e689c7867c007685c98855b8c
Chaitya <chaitanyaasole@gmail.com>
2024-04-24 17:02:58 +0530

    Added login of javascript

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 let string = "";
+
+// ! Select all by using queySelectorAll using 'button' class
 let buttons = document.querySelectorAll('button');
 
-Array.from(buttons).forEach((button)=>{
-        button.addEventListener('click', (e)=> {
-            console.log(e.target)
-        })
+
+Array.from(buttons).forEach((buttons) => {
+
+    buttons.addEventListener('click',(e) => 
+    {
+        if(e.target.innerHTML == '=')
+        {
+            string = eval(string);
+            document.querySelector('input').value = string;
+        }
+        else if(e.target.innerHTML == 'C')
+        {
+            string = "";
+            document.querySelector('input').value = string;
+        }
+        else
+        {
+            console.log(e.target.innerHTML)
+            string = string + e.target.innerHTML;
+            document.querySelector('input').value = string;
+        }
+    })
 })
